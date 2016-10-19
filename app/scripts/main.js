3d1b7ab9b0d4563e3a455749a0e61f353c20debd
Andrey Golovin <caldwell@inbox.ru>
2016-10-20 02:08:01 +0500

    сделал слайдер для картинок

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,44 +2,40 @@
 
 $(function () {
 
-    // таймер обратного отсчета
-    var $timer = $('#timer');
-    $timer
-        .countdown($timer.data('date') || '2016-10-28')
-        .on('update.countdown', function(event) {
-            var template = '<div class="square days"><div class="number">%D</div><div class="title">дней</div></div>'
-                +'<div class="square hours">'
-                +'<div class="number">%H</div>'
-                +'<div class="title">часов</div>'
-                +'</div>'
-                +'<div class="square minutes">'
-                +'<div class="number">%M</div>'
-                +'<div class="title">минут</div>'
-                +'</div>'
-                +'<div class="square seconds">'
-                +'<div class="number">%S</div>'
-                +'<div class="title">секунд</div>'
-                +'</div>'
-                +'<div class="timer_title title_left">До начала битвы осталось</div>'
-                +'<div class="timer_title title_right">Успей получить подарок!</div>'
-            var $this = $(this).html(event.strftime(template));
-        });
+  $('#slider').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>'
+  });
 
-    $(document).on('submit', '[rel=subscribe-form]', function() {
-        var $form = $(this);
-        var data = $form.serialize();
-        var url = $form.attr('action');
-        $
-        .get(url, data)
-        .done(function(){
-            alert('Поздравляем');
-            $form.find('[name=name]').val('');
-            $form.find('[name=email]').val('');
-        });
-        return false;
->>>>>>> c87ae0910d8efa19e0d4fd0b643c929922d4f963
+  // таймер обратного отсчета
+  var $timer = $('#timer');
+  $timer
+    .countdown($timer.data('date') || '2016-10-28')
+    .on('update.countdown', function (event) {
+      var template = `
+        <div class="square days">
+          <div class="number">%D</div>
+          <div class="title">дней</div>
+        </div>
+        <div class="square hours">
+          <div class="number">%H</div>
+          <div class="title">часов</div>
+        </div>
+        <div class="square minutes">
+          <div class="number">%M</div>
+          <div class="title">минут</div>
+        </div>
+        <div class="square seconds">
+          <div class="number">%S</div>
+          <div class="title">секунд</div>
+        </div>
+        <div class="timer_title title_left">До начала битвы осталось</div>
+        <div class="timer_title title_right">Успей получить подарок!</div>`;
+      var $this = $(this).html(event.strftime(template));
     });
-
 
   // сабмит формы
   $(document).on('submit', '[rel=subscribe-form]', function () {
