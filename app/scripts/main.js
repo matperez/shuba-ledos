6ec5088354827fef526a70bfe798cf8d1ffebdc4
Andrey Golovin <caldwell@inbox.ru>
2016-10-19 19:10:58 +0500

    new

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$(function() {
+
+    $(document).on('submit', '[rel=customer-form]', function() {
+        var $form = $(this);
+        var data = $form.serialize();
+        alert(data);
+        return false;
+    });
+
+    
+});
